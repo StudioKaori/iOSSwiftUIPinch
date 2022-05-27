74c7229b7ccb56be3a84fd08683a8d13f7448576
Studio Kaori <studiokaori.com@gmail.com>
2022-05-27 20:36:40 +0200

    Add: animation to image

--- SwiftUIPinch/Screen/ContentView.swift
+++ SwiftUIPinch/Screen/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Property
+    
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - function
+    
+    // MARK: - content
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -18,9 +26,14 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                isAnimating = true
+            })
         } //: NavigationView
         .navigationViewStyle(.stack)
         
