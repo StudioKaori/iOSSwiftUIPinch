79ea8953817ae53448209c45c33a14513ed5ca60
Studio Kaori <studiokaori.com@gmail.com>
2022-05-27 19:50:45 +0200

    Initial Commit

---
+++ SwiftUIPinch/SwiftUIPinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPinchApp.swift
+//  SwiftUIPinch
+//
+//  Created by Kaori Persson on 2022-05-27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
