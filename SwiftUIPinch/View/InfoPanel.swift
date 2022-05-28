7c3a3476fe9d505a016c1145c4593bce3e8bca08
Studio Kaori <studiokaori.com@gmail.com>
2022-05-28 07:36:19 +0200

    Add: InfoPanel view

---
+++ SwiftUIPinch/View/InfoPanel.swift
@@ -0,0 +1,23 @@
+//
+//  InfoPanel.swift
+//  SwiftUIPinch
+//
+//  Created by Kaori Persson on 2022-05-28.
+//
+
+import SwiftUI
+
+struct InfoPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoPanel_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanel()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
