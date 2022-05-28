ac634d27f68d6435681e118c482c7e31a0dc68a9
Studio Kaori <studiokaori.com@gmail.com>
2022-05-28 07:55:22 +0200

    Add: long press gesture

--- SwiftUIPinch/View/InfoPanel.swift
+++ SwiftUIPinch/View/InfoPanel.swift
@@ -11,12 +11,19 @@ struct InfoPanel: View {
     var scale: CGFloat
     var offset: CGSize
     
+    @State private var isInfoPanelVisible: Bool = false
+    
     var body: some View {
         HStack {
             // MARK: - hotspot
             Image(systemName: "circle.circle")
                 .symbolRenderingMode(.hierarchical)
                 .frame(width: 30, height: 30)
+                .onLongPressGesture(minimumDuration: 1) {
+                    withAnimation(.easeOut) {
+                        isInfoPanelVisible.toggle()
+                    }
+                }
             
             // MARK: - info panel
             HStack(spacing: 2) {
@@ -41,6 +48,7 @@ struct InfoPanel: View {
             .background(.ultraThinMaterial)
             .cornerRadius(8)
             .frame(maxWidth: 420)
+            .opacity(isInfoPanelVisible ? 1 : 0)
             
             Spacer()
         }
