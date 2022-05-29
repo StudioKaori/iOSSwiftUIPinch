e03492bf477af2a4b5196aefe1b5fbcc219ad052
Studio Kaori <studiokaori.com@gmail.com>
2022-05-29 07:20:20 +0200

    Add: PageModel

---
+++ SwiftUIPinch/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  SwiftUIPinch
+//
+//  Created by Kaori Persson on 2022-05-29.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
